2bad4c9b7d09fd25c3df4c8930f50298a5000544
Filip Havrlent <16169571+fhavrlent@users.noreply.github.com>
2021-10-18 23:14:22 +0200

    Update first.sh

--- first.sh
+++ first.sh
@@ -1,41 +1,5 @@
 #!/bin/bash
 
-# Update system clock
-timedatectl set-ntp true
-
-# Create 1 EFI system partition and 1 LVM partition on nvme0n1
-
-nvme format -s1 /dev/nvme0n1
-nvme format -s1 /dev/nvme1n1
-fdisk /dev/nvme0n1 <<EEOF
-g
-n
-
-
-+512M
-t
-1
-n
-
-
-
-t
-
-30
-w
-EEOF
-
-
-# Create 1 LVM partition on nvme1n1
-fdisk /dev/nvme1n1 <<EEOF
-g
-n
-
-
-
-t
-w
-EEOF
 
 
 # Format partition nvme0n1p1 to FAT32
@@ -59,14 +23,14 @@ mkfs.ext4 /dev/vol1/root
 mkfs.ext4 /dev/vol2/home
 
 # Mount partitions
-mkdir /mnt/home
-mkdir /mnt/boot
 mount /dev/vol1/root /mnt
-mount /dev/vol2/root /mnt/home
+mkdir /mnt/home
+mount /dev/vol2/home /mnt/home
+mkdir /mnt/boot
 mount /dev/nvme0n1p1
 
-genfstab -U /mnt >> /mnt/etc/fstab
-
 pacstrap -i /mnt base base-devel linux linux-headers linux-lts linux-lts-headers linux-firmware lvm2 nano wget
+
+genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
